86235cfb2337022550b5ea6c34818d55b26e92bf
clusteradmin <clusteradmin@yavuz.com>
2025-11-15 14:42:43 -0800

    Adjusted script so that it lists ALL databases in case no database option is provided

--- showBackups.py
+++ showBackups.py
@@ -9,9 +9,10 @@ except Exception as e:
 @click.version_option('0.01', prog_name='Backup Lister')
 @click.option('--hostname', '-H', help='SQL Server host (use "." for localhost)', type=str, default='.', show_default=True)
 @click.option('--instance', '-i', help='Instance name (leave empty for default instance)', type=str, default='', show_default=True)
-@click.option('--database', '-d', help='Database you want to see backups for', type=str, default='master', show_default=True)
+@click.option('--database', '-d', help='Database you want to see backups for (omit to show all databases)', type=str, default=None, show_default=False)
 def main(hostname, instance, database):
-    """Show backups for a given database by querying msdb.dbo.backupset / backupmediafamily"""
+    """Show backups for a given database by querying msdb.dbo.backupset / backupmediafamily.
+    If `--database` is omitted, list backups for all databases in the instance."""
     if pyodbc is None:
         click.echo("pyodbc is not installed. Install with: pip install pyodbc", err=True)
         sys.exit(1)
@@ -25,7 +26,7 @@ def main(hostname, instance, database):
     driver = next((d for d in drivers if 'ODBC Driver' in d or 'SQL Server' in d), drivers[0])
 
     conn_str = f"DRIVER={{{driver}}};SERVER={server};DATABASE=msdb;Trusted_Connection=yes;"
-    query = """
+    base_query = """
     SELECT
       b.database_name,
       b.backup_start_date,
@@ -42,16 +43,23 @@ def main(hostname, instance, database):
       dbo.backupset b
     INNER JOIN
       dbo.backupmediafamily m ON b.media_set_id = m.media_set_id
-    WHERE
-      b.database_name = ?
-    ORDER BY
-      b.backup_start_date DESC;
     """
+
+    # If a specific database was requested, filter by it; otherwise show all databases
+    if database:
+        query = base_query + "\n    WHERE\n      b.database_name = ?\n    ORDER BY\n      b.backup_start_date DESC;"
+        params = (database,)
+    else:
+        query = base_query + "\n    ORDER BY\n      b.database_name, b.backup_start_date DESC;"
+        params = None
 
     try:
         with pyodbc.connect(conn_str, autocommit=True) as conn:
             cur = conn.cursor()
-            cur.execute(query, database)
+            if params:
+                cur.execute(query, params)
+            else:
+                cur.execute(query)
             cols = [col[0] for col in cur.description] if cur.description else []
             rows = cur.fetchall()
             # print header
